08523adacb7ff4c8ce74055908e0bbc8c19cc7c2
Patrick NGUYEN <p.nguyen@live.fr>
2022-06-26 21:55:18 +0200

    Correction Fond d'écran du site

--- plate-forme-vod/src/Components/LoginScreenComponent/LoginScreen.jsx
+++ plate-forme-vod/src/Components/LoginScreenComponent/LoginScreen.jsx
@@ -5,7 +5,7 @@ import SignUpScreen from '../SignUpScreenComponent/SignUpScreen';
 const LoginScreen = () => {
     const [signIn, setSignIn] = useState(false);
     return (
-        <div className=".loginScreen">
+        <div className="loginScreen">
             <img className="loginScreen_logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/640px-Netflix_2015_logo.svg.png"
                 alt="Logo Netflix"
