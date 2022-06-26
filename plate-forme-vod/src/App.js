dad90b182a073be0c67c288ab2b6f2c72cc22f07
Patrick NGUYEN <p.nguyen@live.fr>
2022-06-26 18:46:57 +0200

    New change structure

--- plate-forme-vod/src/App.js
+++ plate-forme-vod/src/App.js
@@ -1,11 +1,11 @@
-import NavBar from './Components/NavBarComponent/NavBar';
-
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      
     </div>
   );
 }
